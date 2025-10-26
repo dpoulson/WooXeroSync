0599f494b17109e3755dd0a1a950b73ebc128fba
Darren Poulson <darren.poulson@gmail.com>
2025-10-26 01:44:11 +0000

    Change id to text field as now encrypted

--- database/migrations/2025_10_23_223943_add_tenant_id_to_users_table.php
+++ database/migrations/2025_10_23_223943_add_tenant_id_to_users_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->string('xero_tenant_id', 36)->nullable();
+            $table->text('xero_tenant_id')->nullable();
         });
     }
 
