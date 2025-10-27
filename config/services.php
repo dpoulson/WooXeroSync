79f941f6fb7bd6d4c5e59d873fc6171086ddde1b
Darren Poulson <darren.poulson@gmail.com>
2025-10-27 23:48:39 +0000

    Fix typo

--- config/services.php
+++ config/services.php
@@ -47,7 +47,7 @@ return [
         'redirect' => '/oauth/github/callback',
     ],
 
-    'github' => [
+    'google' => [
         'client_id' => env('GOOGLE_CLIENT_ID'),
         'client_secret' => env('GOOGLE_CLIENT_SECRET'),
         'redirect' => '/oauth/google/callback',
